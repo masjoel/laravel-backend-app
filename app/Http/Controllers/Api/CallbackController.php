5b3bc710f60ff34b7a70444a80670a300b5084c5
Masjoel <masjoel@gmail.com>
2023-08-27 20:55:58 +0700

    update CallbackService

--- app/Http/Controllers/Api/CallbackController.php
+++ app/Http/Controllers/Api/CallbackController.php
@@ -9,43 +9,43 @@ use Illuminate\Http\Request;
 
 class CallbackController extends Controller
 {
-    // public function callback()
-    // {
-    //     $callback = new CallbackService;
+    public function callback()
+    {
+        $callback = new CallbackService;
 
-    //     // if ($callback->isSignatureKeyVerified()) {
-    //     $notification = $callback->getNotification();
-    //     $order = $callback->getOrder();
+        if ($callback->isSignatureKeyVerified()) {
+            $notification = $callback->getNotification();
+            $order = $callback->getOrder();
 
-    //     if ($callback->isSuccess()) {
-    //         Order::where('id', $order->id)->update([
-    //             'payment_status' => 2,
-    //         ]);
-    //     }
+            if ($callback->isSuccess()) {
+                Order::where('id', $order->id)->update([
+                    'payment_status' => 2,
+                ]);
+            }
 
-    //     if ($callback->isExpire()) {
-    //         Order::where('id', $order->id)->update([
-    //             'payment_status' => 3,
-    //         ]);
-    //     }
+            if ($callback->isExpire()) {
+                Order::where('id', $order->id)->update([
+                    'payment_status' => 3,
+                ]);
+            }
 
-    //     if ($callback->isCancelled()) {
-    //         Order::where('id', $order->id)->update([
-    //             'payment_status' => 3,
-    //         ]);
-    //     }
+            if ($callback->isCancelled()) {
+                Order::where('id', $order->id)->update([
+                    'payment_status' => 3,
+                ]);
+            }
 
-    //     return response()
-    //         ->json([
-    //             'success' => true,
-    //             'message' => 'Notification successfully processed',
-    //         ]);
-    //     // } else {
-    //     //     return response()
-    //     //         ->json([
-    //     //             'error' => true,
-    //     //             'message' => 'Signature key not verified',
-    //     //         ], 403);
-    //     // }
-    // }
+            return response()
+                ->json([
+                    'success' => true,
+                    'message' => 'Notification successfully processed',
+                ]);
+        } else {
+            return response()
+                ->json([
+                    'error' => true,
+                    'message' => 'Signature key not verified',
+                ], 403);
+        }
+    }
 }
