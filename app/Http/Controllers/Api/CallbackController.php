3b752025e0b05eada431739a69a7c7129e2b070e
Masjoel <masjoel@gmail.com>
2023-08-28 08:38:26 +0700

    update Callbacks

--- app/Http/Controllers/Api/CallbackController.php
+++ app/Http/Controllers/Api/CallbackController.php
@@ -2,18 +2,45 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Order;
-use App\Services\Midtrans\CallbackService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use App\Http\Controllers\Controller;
+use App\Models\OrderItem;
+use App\Services\Midtrans\CallbackService;
 
 class CallbackController extends Controller
 {
+
+    // public function __construct()
+    // {
+    //     $callback = new CallbackService;
+    //     $this->notification = $callback->getNotification();
+    //     $this->order = $callback->getOrder();
+    //     $this->orderItem  = Order::where('number', $this->order->number)->with('orderItems')->with('orderItems.product')->get();
+    //     $this->serverKey = config('midtrans.server_key');
+    // }
+    // private function _createLocalsignatureKey()
+    // {
+    //     $total = new Collection();
+    //     foreach ($this->orderItem[0]->orderItems as $item) {
+    //         $total->push([
+    //             'total' => $item->product->price * $item->quantity,
+    //         ]);
+    //     }
+    //     $orderId = $this->order->number;
+    //     $statusCode = $this->notification->status_code;
+    //     $grossAmount = number_format($total->sum('total'), 2, '.', '');
+    //     $serverkey = $this->serverKey;
+    //     $input = $orderId . $statusCode . $grossAmount . $serverkey;
+    //     $signature = openssl_digest($input, 'sha512');
+    //     return $signature;
+    // }
     public function callback()
     {
+        // $sign = $this->_createLocalsignatureKey();
         $callback = new CallbackService;
-
-        // if ($callback->isSignatureKeyVerified()) {
+        if ($callback->isSignatureKeyVerified()) {
             $notification = $callback->getNotification();
             $order = $callback->getOrder();
 
@@ -40,12 +67,12 @@ class CallbackController extends Controller
                     'success' => true,
                     'message' => 'Notification successfully processed',
                 ]);
-        // } else {
-        //     return response()
-        //         ->json([
-        //             'error' => true,
-        //             'message' => 'Signature key not verified',
-        //         ], 403);
-        // }
+        } else {
+            return response()
+                ->json([
+                    'error' => true,
+                    'message' => 'Signature key not verified',
+                ], 403);
+        }
     }
 }
