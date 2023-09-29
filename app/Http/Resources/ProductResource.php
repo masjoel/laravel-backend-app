3b664caa1492d4f481ebb8d41e0eeedd6a3f861a
Masjoel <masjoel@gmail.com>
2023-09-29 08:37:21 +0700

    update product filter by cat, show detail cat

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -20,7 +20,7 @@ class ProductResource extends JsonResource
             'descripton' => $this->description,
             'price' => $this->price,
             'image_product' => $this->image_url,
-            'category_id' => $this->category_id,
+            'category_id' => new CategoryResource($this->whenLoaded('category')),
             'user' => new UserResource($this->whenLoaded('user')),
         ];
     }
