3848c87e0dfa4c16d514be734e265bef9b38af70
Masjoel <masjoel@gmail.com>
2023-11-18 15:45:32 +0700

    update order ctrl

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -75,7 +75,7 @@ class OrderController extends Controller
             fn ($query, $payment_status) => $query->where('payment_status', '=', $payment_status)
         )
             ->get();
-        $order->load('orderItems', 'user');
+        // $order->load('orderItems', 'user');
         return new OrderResource($order);
     }
 }
